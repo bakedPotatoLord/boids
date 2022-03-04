b5e3eb9e6bd064d01cb3494109e00fa0e604e542
josiah <918780@dpsk12.net>
2022-03-03 17:38:23 -0700

    got a basic functional version with no rules

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ var closeBirds = []
 
 function init(){
 	for(var i = 0; i<birdNum;i++){
-		birdArray.push(new Bird(rand(0,cw),rand(0,ch),rand(0,2*Math.PI)))
+		birdArray.push(new Bird(rand(0,cw),rand(0,ch),rand(-1,1),rand(1,-1)))
 	}
 	//starts loop
 	loop()
@@ -29,52 +29,30 @@ function update(){
 	//seperation with walls
 	for (i of birdArray) {
 		//left sightPath
-		if(isOffscreen(i.x+(viewDist*Math.cos(i.r-fov)),i.y+(viewDist*Math.sin(i.r-fov))) ){
-			i.r+=0.1+(Math.random()*0.1)
-		}else if(isOffscreen(i.x+(viewDist*Math.cos(i.r+fov)),i.y+(viewDist*Math.sin(i.r+fov)))){
-			i.r-=0.1+(Math.random()*0.1)
+		if(i.x+(20*i.xv) <0){
+
 		}
 
 		//define close bird array
-		closeBirds = {x:[],y:[],r:[],dists:[],avgDist:0,}
+		closeBirds = []
 		for(i0 of birdArray){
 			if(dist(i.x,i.y,i0.x,i0.y) <= cohortDist ){
-				closeBirds.x.push(i0.x)
-				closeBirds.y.push(i0.y)
-				closeBirds.r.push(i0.r)
+				closeBirds.push(i0)
 			}
 		}
-		//apply allignment
-		i.r=(i.r+ (closeBirds.r.reduce((partialSum, a) => partialSum + a, 0)/(closeBirds.r.length)) )/2
+		
 
 		//apply seperation
 		for(i0 in closeBirds.x){
-			closeBirds.dists.push(dist(i.x,i.y,closeBirds.x[i0],closeBirds.y[i0]))
+			
 		}
-		closeBirds.avgDist = mean(closeBirds.dists)
-		for(i0 in closeBirds.x){
-			if(dist(i.x,i.y,closeBirds.x[i0],closeBirds.y[i0])>closeBirds.avgDist){
-				if(closeBirds.y[i0]-i.y > Math.tan(i.r)*(closeBirds.x[i0]-i.x )){
-					if(i.r>=(Math.PI/2)&&i.r<=(Math.PI*(3/2))){
-						i.r+=0.05
-					}else{
-						i.r-=0.05
-					}
-				}else{
-					if(i.r>=(Math.PI/2)&&i.r<=(Math.PI*(3/2))){
-						i.r-=0.05
-					}else{
-						i.r+=0.05
-					}
-				}
-			}
-		}
+		
 	}
 
 	//update xy position based on r val
 	for( i in birdArray){
-		birdArray[i].x +=Math.cos(birdArray[i].r)
-		birdArray[i].y +=Math.sin(birdArray[i].r)
+		birdArray[i].x +=birdArray[i].xv
+		birdArray[i].y +=birdArray[i].yv
 	}
 }
 
@@ -86,7 +64,7 @@ function draw(){
 	ctx.lineWidth = 2
 	for( i of birdArray){
 		ctx.beginPath();
-		ctx.ellipse(i.x, i.y, 5, 3, i.r, 0, 2*Math.PI);
+		ctx.ellipse(i.x, i.y, 5, 3, Math.atan(i.yv/i.xv), 0, 2*Math.PI);
 		ctx.fill()
 		if(showLines){
 			line(i.x,i.y,i.x+(viewDist*Math.cos(i.r)),i.y+(viewDist*Math.sin(i.r)))
