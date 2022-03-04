30e0a3c140f5c145a087ad0ac5342a88b072f815
josiah <918780@dpsk12.net>
2022-03-03 17:26:35 -0700

    moved unrelated functions to lib.js

--- script.js
+++ script.js
@@ -13,35 +13,9 @@ const cohortDist = 20
 const showLines = false;
 
 var birdArray = [];
-var closeBirds = {x:[],y:[],r:[],}
+var closeBirds = []
 
-function rand(min,max){
-	return min+(Math.random()*(max-min))
-}
 
-function line(x1,y1,x2,y2){
-	ctx.beginPath();
-	ctx.moveTo(x1, y1);
-	ctx.lineTo(x2, y2); 
-	ctx.stroke();  
-}
-
-function isOffscreen(x,y){
-	//return(x <0||x>cw||y<0||y>ch)
-	return(Math.sqrt( ( ( x-200 )**2 ) + ( (y-200 )**2 )) >200)
-}
-
-function dist(x1,y1,x2,y2){
-	return Math.sqrt(((x1-x2)**2) + ( (y1-y2)**2) )
-}
-
-function sum(arr){
- return arr.reduce((partialSum, a) => partialSum + a, 0);
-}
-
-function mean(arr){
-	return sum(arr)/arr.length
-}
 
 function init(){
 	for(var i = 0; i<birdNum;i++){
@@ -135,10 +109,11 @@ function loop(){
 }
 
 class Bird{
-	constructor(x,y,r){
-		this.x = x
-		this.y = y
-		this.r = r
+	constructor(x,y,xv,yv){
+		this.x = x;
+		this.y = y;
+		this.xv = xv;
+		this.yv = yv;
 	}
 }
 
