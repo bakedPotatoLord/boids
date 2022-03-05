4c23c1a2ea20936343b7d8c3fa95fb3394c271c9
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2022-03-05 03:55:56 +0000

    revert to rotation base

--- script.js
+++ script.js
@@ -13,13 +13,39 @@ const cohortDist = 20
 const showLines = false;
 
 var birdArray = [];
-var closeBirds = []
+var closeBirds = {x:[],y:[],r:[],}
 
+function rand(min,max){
+	return min+(Math.random()*(max-min))
+}
 
+function line(x1,y1,x2,y2){
+	ctx.beginPath();
+	ctx.moveTo(x1, y1);
+	ctx.lineTo(x2, y2); 
+	ctx.stroke();  
+}
+
+function isOffscreen(x,y){
+	//return(x <0||x>cw||y<0||y>ch)
+	return(Math.sqrt( ( ( x-200 )**2 ) + ( (y-200 )**2 )) >200)
+}
+
+function dist(x1,y1,x2,y2){
+	return Math.sqrt(((x1-x2)**2) + ( (y1-y2)**2) )
+}
+
+function sum(arr){
+ return arr.reduce((partialSum, a) => partialSum + a, 0);
+}
+
+function mean(arr){
+	return sum(arr)/arr.length
+}
 
 function init(){
 	for(var i = 0; i<birdNum;i++){
-		birdArray.push(new Bird(rand(0,cw),rand(0,ch),rand(-1,1),rand(1,-1)))
+		birdArray.push(new Bird(rand(0,cw),rand(0,ch),rand(0,2*Math.PI)))
 	}
 	//starts loop
 	loop()
@@ -29,49 +55,52 @@ function update(){
 	//seperation with walls
 	for (i of birdArray) {
 		//left sightPath
-		if(i.x+(20*i.xv) <0){
-			i.xv= 1
-		}
-		if(i.x+(20*i.xv) >cw){
-			i.xv = -1
-
-		}
-		if(i.y+(20*i.yv) <0){
-			i.yv = 1
-
-		}
-		if(i.y+(20*i.yv) >cw){
-			i.yv = -1
-			
+		if(isOffscreen(i.x+(viewDist*Math.cos(i.r-fov)),i.y+(viewDist*Math.sin(i.r-fov))) ){
+			i.r+=0.1+(Math.random()*0.1)
+		}else if(isOffscreen(i.x+(viewDist*Math.cos(i.r+fov)),i.y+(viewDist*Math.sin(i.r+fov)))){
+			i.r-=0.1+(Math.random()*0.1)
 		}
 
 		//define close bird array
-		closeBirds = []
+		closeBirds = {x:[],y:[],r:[],dists:[],avgDist:0,}
 		for(i0 of birdArray){
-			if(dist(i.x,i.y,i0.x,i0.y) <= cohortDist/2 ){
-				closeBirds.push(i0)
+			if(dist(i.x,i.y,i0.x,i0.y) <= cohortDist ){
+				closeBirds.x.push(i0.x)
+				closeBirds.y.push(i0.y)
+				closeBirds.r.push(i0.r)
 			}
 		}
-		
-		
-		//apply seperation
-		for(i0 of closeBirds){
-			i.xv -= (i.x -i0.x)/20
-			i.yv -= (i.y -i0.y)/20
-		}
-
 		//apply allignment
-		for(i0 of closeBirds){
-			i.xv = (20*i.xv +i0.xv)/21
-			i.yv = (20*i.yv +i0.yv)/21
+		i.r=(i.r+ (closeBirds.r.reduce((partialSum, a) => partialSum + a, 0)/closeBirds.r.length) )/2
+
+		//apply seperation
+		for(i0 in closeBirds.x){
+			closeBirds.dists.push(dist(i.x,i.y,closeBirds.x[i0],closeBirds.y[i0]))
 		}
-		
+		closeBirds.avgDist = mean(closeBirds.dists)
+		for(i0 in closeBirds.x){
+			if(dist(i.x,i.y,closeBirds.x[i0],closeBirds.y[i0])>closeBirds.avgDist){
+				if(closeBirds.y[i0]-i.y > Math.tan(i.r)*(closeBirds.x[i0]-i.x )){
+					if(i.r>=(Math.PI/2)&&i.r<=(Math.PI*(3/2))){
+						i.r+=0.1
+					}else{
+						i.r-=0.1
+					}
+				}else{
+					if(i.r>=(Math.PI/2)&&i.r<=(Math.PI*(3/2))){
+						i.r-=0.1
+					}else{
+						i.r+=0.1
+					}
+				}
+			}
+		}
 	}
 
 	//update xy position based on r val
-	for( i of birdArray){
-		i.x +=i.xv
-		i.y +=i.yv
+	for( i in birdArray){
+		birdArray[i].x +=Math.cos(birdArray[i].r)
+		birdArray[i].y +=Math.sin(birdArray[i].r)
 	}
 }
 
@@ -83,7 +112,7 @@ function draw(){
 	ctx.lineWidth = 2
 	for( i of birdArray){
 		ctx.beginPath();
-		ctx.ellipse(i.x, i.y, 5, 3, Math.atan(i.yv/i.xv), 0, 2*Math.PI);
+		ctx.ellipse(i.x, i.y, 5, 3, i.r, 0, 2*Math.PI);
 		ctx.fill()
 		if(showLines){
 			line(i.x,i.y,i.x+(viewDist*Math.cos(i.r)),i.y+(viewDist*Math.sin(i.r)))
@@ -106,11 +135,10 @@ function loop(){
 }
 
 class Bird{
-	constructor(x,y,xv,yv){
-		this.x = x;
-		this.y = y;
-		this.xv = xv;
-		this.yv = yv;
+	constructor(x,y,r){
+		this.x = x
+		this.y = y
+		this.r = r
 	}
 }
 
